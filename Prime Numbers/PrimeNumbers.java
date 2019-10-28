ae1481664d1fc0d5031edbfa8067bef72f43693f
Jaeheon Shim <35268332+jaeheonshim@users.noreply.github.com>
2019-10-28 09:03:57 -0400

    Fix "less than ideal" formatting

--- Prime Numbers/PrimeNumbers.java
+++ Prime Numbers/PrimeNumbers.java
@@ -1,29 +1,23 @@
 import java.util.*;
- 
-class PrimeNumbers
-{
-   public static void main(String args[])
-   {
-      int status = 1, count, j;
-      long num = 3L;
-     
-      for (count = 2;;)
-      {
-         for (j = 2; j <= Math.sqrt(num); j++)
-         {
-            if (num%j == 0)
-            {
-               status = 0;
-               break;
+
+class PrimeNumbers {
+    public static void main(String args[]) {
+        int status = 1, count, j;
+        long num = 3L;
+
+        for (count = 2; ; ) {
+            for (j = 2; j <= Math.sqrt(num); j++) {
+                if (num % j == 0) {
+                    status = 0;
+                    break;
+                }
             }
-         }
-         if (status != 0)
-         {
-            System.out.println(num);
-            count++;
-         }
-         status = 1;
-         num++;
-      }        
-   }
+            if (status != 0) {
+                System.out.println(num);
+                count++;
+            }
+            status = 1;
+            num++;
+        }
+    }
 }
